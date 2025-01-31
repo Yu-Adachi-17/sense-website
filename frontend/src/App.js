50ff47e7f155b980704a6cb6d44c11170c98df27
Yu-Adachi-17 <t.au.1717@gmail.com>
2025-01-31 12:31:01 +0900

    Force update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -238,6 +238,7 @@ function App() {
         />
 
         {/* 決済後のページ */}
+        <Route path="/" element={<Home />} /> 
         <Route path="/success" element={<Success />} />
         <Route path="/cancel" element={<Cancel />} />
       </Routes>
