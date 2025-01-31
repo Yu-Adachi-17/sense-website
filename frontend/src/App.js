f52c4ca02c3a18d049e7969179466ac688522c66
Yu-Adachi-17 <t.au.1717@gmail.com>
2025-01-31 11:51:41 +0900

    Force update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import FullScreenOverlay from './components/FullScreenOverlay.js';
 import ProgressIndicator from './components/ProgressIndicator';
 import { transcribeAudio } from './utils/ChatGPTs';
 import { Success, Cancel } from './AfterPayment'; // 成功画面とキャンセル画面の名前付きインポート
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'; // CSSファイルをインポート
 
 function App() {
