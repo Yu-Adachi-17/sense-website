76cb00c5b4f8df93dd01918cac59721cf689e66e
Yu-Adachi-17 <t.au.1717@gmail.com>
2025-02-07 12:10:11 +0900

    Force update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -403,8 +403,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/success" element={<Success />} />
         <Route path="/cancel" element={<Cancel />} />
-        <Route path="/minutes-list" element={<MinutesList />} />
-        <Route path="/minutes/:id" element={<MinutesDetail />} />
+        <Route path="/minutes" element={<MinutesList />} />
+        {/* ここで :id が渡される */}
+        <Route path="/minutes/:id" element={<FullScreenOverlay />} />
         <Route path="*" element={<h1 style={{ color: "white", textAlign: "center" }}>404 Not Found</h1>} />
       </Routes>
     </Router>
