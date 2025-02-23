0ab2cabf58013eb4b3fe7f8a2ca401073118f538
Yu-Adachi-17 <t.au.1717@gmail.com>
2025-02-23 19:09:37 +0900

    Force update

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -5,16 +5,14 @@ import { initReactI18next } from 'react-i18next';
 import en from './locales/en.json';
 import ja from './locales/ja.json';
 
-i18n
-  .use(initReactI18next) // React 用に i18next を初期化
-  .init({
-    resources: {
-      en: { translation: en },
-      ja: { translation: ja },
-    },
-    lng: 'en', // デフォルトの言語
-    fallbackLng: 'en', // 言語が見つからない場合のフォールバック
-    interpolation: { escapeValue: false }, // React の XSS 対策は React 自体がやってくれるので false
-  });
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    ja: { translation: ja }
+  },
+  lng: 'ja', // ✅ デフォルト言語を日本語に設定
+  fallbackLng: 'en',
+  interpolation: { escapeValue: false }
+});
 
 export default i18n;
