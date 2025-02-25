707d22d36cf8c489f0512fab308cc808eeb4b0c6
Yu-Adachi-17 <t.au.1717@gmail.com>
2025-02-25 10:56:03 +0900

    Force update

--- frontend/src/components/SignUpSuccess.jsx
+++ frontend/src/components/SignUpSuccess.jsx
@@ -25,10 +25,10 @@ export function SignUpSuccess() {
         fontFamily: "Impact, sans-serif",
       }}
     >
-      <h1 style={{ fontWeight: 300, letterSpacing: "0.05em" }}>
+      <h1 style={{ fontWeight: 225, letterSpacing: "0.05em" }}>
         Sign Up Successful
       </h1>
-      <p style={{ fontSize: "0.8em", marginTop: "10px" }}>
+      <p style={{ fontSize: "0.6em", marginTop: "10px" }}>
         Redirecting to home in a few seconds...
       </p>
     </div>
