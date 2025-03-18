098ed4e7479ea68032b56b2902abd74c20e080c4
Yu-Adachi-17 <t.au.1717@gmail.com>
2025-03-18 13:29:06 +0900

    Force update

---
+++ src/src_backup/pages/_app.js
@@ -0,0 +1,18 @@
+import '../App.css'; // ✅ グローバルCSSをここで読み込む
+import '../News.css';
+import '../i18n/index'; // ✅ i18next の初期化
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+
+function MyApp({ Component, pageProps }) {
+  const { i18n } = useTranslation();
+
+  // アラビア語対応
+  useEffect(() => {
+    document.documentElement.setAttribute("dir", i18n.language === "ar" ? "rtl" : "ltr");
+  }, [i18n.language]);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
