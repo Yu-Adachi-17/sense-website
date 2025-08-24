e42fc403e2753a0301242df3a9559da0d8d66395
Yu-Adachi-17 <t.au.1717@gmail.com>
2025-08-24 12:41:58 +0900

    Force update

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -665,7 +665,7 @@ function App() {
               left: '2px',
               right: '2px',
               bottom: '2px',
-              background: 'black',
+              background: 'white',
               borderRadius: '38px',
             }} />
             <div style={{
