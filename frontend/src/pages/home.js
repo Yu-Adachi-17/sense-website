6ec0a611623b4f74ee8f104b9aef18fb2cfef9a1
Yu-Adachi-17 <t.au.1717@gmail.com>
2025-11-17 10:17:06 +0900

    Force update

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -588,7 +588,7 @@ export default function Home() {
                 {t("Blog")}
               </Link>
               <Link href="/company" className="navLink">
-                {t("Company")}
+                Company
               </Link>
             </nav>
 
